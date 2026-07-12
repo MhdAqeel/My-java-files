86592cb8a28629d1f09460bf77ca1600d4ecb51d
MhdAqeel <mohamedaqeel1130@gmail.com>
2026-07-12 20:34:42 +0530

    second commit.

--- hackathon/MaximumSubarraySum.java
+++ hackathon/MaximumSubarraySum.java
@@ -1,29 +1,24 @@
 import java.util.*;
-public class MaximumSubarraySum{
+
+public class MaximumSubarraySum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
-        int [] arr = new int[length];
-        for(int i = 0; i<length; i++){
-            arr[i]=sc.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
         }
-        int maxSum = arr[0];
+
         int currentSum = arr[0];
+        int maxSum = arr[0];
+
         for (int i = 1; i < arr.length; i++) {
-            if(currentSum + arr[i] > maxSum){
-                maxSum = currentSum + arr[i];
-            }
-            if(currentSum + arr[i] < arr[i] ){
-                currentSum = arr[i];
-            }
-            else{
-                currentSum += arr[i];
-            }
-
-
+            // Step 1: decide whether to extend or restart
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            // Step 2: THEN check if this new currentSum beats maxSum
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        System.out.println("max sum: "+maxSum);
-
+        System.out.println("max sum: " + maxSum);
     }
 }
